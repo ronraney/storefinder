9645cfe1ef781c7877b24f32bb94e7e9e88d0fb6
ronraney <raneyron@yahoo.com>
2018-05-18 15:10:02 -0500

    Sixth commit

---
+++ app/main.component.js
@@ -0,0 +1,54 @@
+// component that decides which main component to load: read or create/update
+var MainApp = React.createClass({
+ 
+    // initial mode is 'read' mode
+    getInitialState: function(){
+        return {
+            currentMode: 'read',
+            pharmacyId: null
+        };
+    },
+ 
+    // used when use clicks something that changes the current mode
+    changeAppMode: function(newMode, pharmacyId){
+        this.setState({currentMode: newMode});
+            if(pharmacyId !== undefined){
+            this.setState({pharmacyId: pharmacyId});
+        }
+    },
+ 
+    // render the component based on current or selected mode
+    render: function(){
+ 
+        var modeComponent =
+            <ReadPharmaciesComponent
+            changeAppMode={this.changeAppMode} />;
+ 
+        switch(this.state.currentMode){
+            case 'read':
+                break;
+            case 'readOne':
+                modeComponent = <ReadOnePharmacyComponent pharmacyId={this.state.pharmacyId} changeAppMode={this.changeAppMode}/>;
+                break;
+            case 'create':
+                modeComponent = <CreatePharmacyComponent changeAppMode={this.changeAppMode}/>;
+                break;
+            case 'update':
+                modeComponent = <UpdatePharmacyComponent pharmacyId={this.state.pharmacyId} changeAppMode={this.changeAppMode}/>;
+                break;
+            case 'delete':
+                modeComponent = <DeletePharmacyComponent pharmacyId={this.state.pharmacyId} changeAppMode={this.changeAppMode}/>;
+                break;
+            default:
+                break;
+        }
+ 
+        return modeComponent;
+    }
+});
+ 
+// go and render the whole React component on to the div with id 'content'
+ReactDOM.render(
+    <MainApp />,
+    document.getElementById('content')
+);
